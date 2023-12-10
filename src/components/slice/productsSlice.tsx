48c6bade9d2f19714c4da76ae07a181b6b6a080d
samettekin01 <samet.tekin.01@gmail.com>
2023-12-10 13:06:30 +0300

    added slider

--- src/components/slice/productsSlice.tsx
+++ src/components/slice/productsSlice.tsx
@@ -2,27 +2,32 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 import { QueryClient } from "react-query";
 import { STATUS } from "../utils/status";
+import { Product } from "../../types/types";
 
 interface InitialState {
-    products: Array<string>,
+    products: Product[],
     productsStatus: string,
-    productDetail?: Array<string>,
+    productDetail?: Product[],
     productDetailStatus: string
-    category: Array<string>,
-    categoryStatus: string
+    category: Product[],
+    categoryStatus: string,
+    sliderProducts: Product[],
+    sliderProductsStatus: string
 }
 
-const initialState = {
+const initialState: InitialState = {
     products: [],
     productsStatus: STATUS.IDLE,
     productDetail: [],
     productDetailStatus: STATUS.IDLE,
     category: [],
-    categoryStatus: STATUS.IDLE
-} as InitialState
+    categoryStatus: STATUS.IDLE,
+    sliderProducts: [],
+    sliderProductsStatus: STATUS.IDLE
+}
 
+const queryClient = new QueryClient()
 export const getProducts = createAsyncThunk("products", async () => {
-    const queryClient = new QueryClient()
     const promise = await queryClient.fetchQuery("products", () =>
         axios.get(`https://fakestoreapi.com/products/`)
     )
@@ -30,18 +35,23 @@ export const getProducts = createAsyncThunk("products", async () => {
 })
 
 export const getDetailProduct = createAsyncThunk("productDetail", async (id: number) => {
-    const queryClient = new QueryClient()
     const promise = await queryClient.fetchQuery("productDetail", () =>
         axios.get(`https://fakestoreapi.com/products/${id}`)
     )
     return promise.data;
 })
 export const getCategory = createAsyncThunk("categories", async (category: string) => {
-    const queryClient = new QueryClient();
     const promise = await queryClient.fetchQuery("categories", () =>
         axios.get(`https://fakestoreapi.com/products/category/${category}`)
     )
-    return promise.data
+    return promise.data;
+})
+
+export const sliderProducts = createAsyncThunk("sliderProducts", async () => {
+    const promise = await queryClient.fetchQuery("sliderProducts", () =>
+        axios.get(`https://fakestoreapi.com/products?limit=5`)
+    )
+    return promise.data;
 })
 
 const productSlice = createSlice({
@@ -50,34 +60,44 @@ const productSlice = createSlice({
     reducers: {},
     extraReducers: (builder) => {
         builder
-            .addCase(getProducts.pending, (state, action) => {
+            .addCase(getProducts.pending, (state) => {
                 state.productsStatus = STATUS.LOADING
             })
             .addCase(getProducts.fulfilled, (state, action) => {
                 state.products = action.payload
                 state.productsStatus = STATUS.SUCCESS
             })
-            .addCase(getProducts.rejected, (state, action) => {
+            .addCase(getProducts.rejected, (state) => {
                 state.productsStatus = STATUS.FAIL
             })
-            .addCase(getDetailProduct.pending, (state, action) => {
+            .addCase(getDetailProduct.pending, (state) => {
                 state.productDetailStatus = STATUS.LOADING
             })
             .addCase(getDetailProduct.fulfilled, (state, action) => {
                 state.productDetail = action.payload
                 state.productDetailStatus = STATUS.SUCCESS
             })
-            .addCase(getDetailProduct.rejected, (state, action) => {
+            .addCase(getDetailProduct.rejected, (state) => {
                 state.productDetailStatus = STATUS.FAIL
             })
-            .addCase(getCategory.pending, (state, action) => {
+            .addCase(getCategory.pending, (state) => {
                 state.categoryStatus = STATUS.LOADING
             })
             .addCase(getCategory.fulfilled, (state, action) => {
                 state.categoryStatus = STATUS.SUCCESS
                 state.category = action.payload
             })
-            .addCase(getCategory.rejected, (state, action) => {
+            .addCase(getCategory.rejected, (state) => {
+                state.categoryStatus = STATUS.FAIL
+            })
+            .addCase(sliderProducts.fulfilled, (state,action) => {
+                state.sliderProducts = action.payload
+                state.sliderProductsStatus = STATUS.SUCCESS
+            })
+            .addCase(sliderProducts.pending, (state) => {
+                state.categoryStatus = STATUS.LOADING
+            })
+            .addCase(sliderProducts.rejected, (state) => {
                 state.categoryStatus = STATUS.FAIL
             })
     }
